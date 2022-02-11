490ec048c658b53fe95f659d44981a695cd5241a
Raagul26 <raagul.thangavelu@trustrace.com>
2022-02-11 13:08:54 +0530

    Created simple server creation app

---
+++ TODO/src/app/server/server.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+
+  id: number = 25
+  serverStatus: string = ""
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline'
+  }
+
+  getServerStatus():string {
+    return this.serverStatus
+  }
+
+  getColor():string {
+    return this.serverStatus === 'online' ? 'green' : 'red'
+  }
+}
